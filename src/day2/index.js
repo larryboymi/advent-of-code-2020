ff16e8cd3a38cc94637a87f1be99d4ff69518bc9
Larry Anderson <larryboymi@hotmail.com>
2020-12-02 08:03:58 -0600

    part 1, day 2

---
+++ src/day2/index.js
@@ -0,0 +1,57 @@
+
+const fs = require('fs')
+const path = require('path')
+const readline = require('readline')
+const filePath = path.join(__dirname, 'input.txt')
+
+const getPwd = line => {
+  const lineSegments = line.split(': ')
+  const policy = lineSegments[0]
+  const password = lineSegments[1]
+  const policySegments = policy.split(' ')
+  const countSegments = policySegments[0].split('-')
+  return {
+    min: countSegments[0],
+    max: countSegments[1],
+    char: policySegments[1],
+    password 
+  }
+}
+
+const getPwdArray = async path => {
+  const pwds = []
+  try {
+    const fileStream = fs.createReadStream(path)
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    })
+    for await (const line of rl) {
+      pwds.push(getPwd(line.trim()))
+    }
+    return pwds
+  } catch( e) {
+    console.error('Error reading file..', e);
+  }
+}
+
+const getOccurrences = (password, char) =>
+  (password.match(new RegExp(char, "g")) || []).length
+
+const numPassing = passwords =>
+  passwords.reduce((acc, curr) => {
+    const occurrences = getOccurrences(curr.password, curr.char)
+    //console.log(`There are ${occurrences} occurrences of ${curr.char} in ${curr.password}`)
+    if (occurrences >= curr.min && occurrences <= curr.max) {
+      return acc + 1
+    }
+    return acc
+  }, 0)
+
+const runDay = async () => {
+  const passwords = await getPwdArray(filePath)
+  //console.log(`passwords = ${JSON.stringify(passwords)}`)
+  console.log(`The number of passwords compliant with policy is ${numPassing(passwords)}`)
+}
+
+runDay()
