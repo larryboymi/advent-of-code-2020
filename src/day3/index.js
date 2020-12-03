357dbc5b4b42248b2bf99143c20de000c1e2be13
Larry Anderson <larryboymi@hotmail.com>
2020-12-03 08:03:40 -0600

    part 2 day 3

--- src/day3/index.js
+++ src/day3/index.js
@@ -1,31 +1,26 @@
 const fs = require("fs")
 const text = fs.readFileSync('input.txt', { encoding: 'utf8' })
-const verticalTravel = 1
-const horizontalTravel = 3
 const treeChar = '#'
 
-const getPatternLocation = ({ vertical, horizontal }, width) => {
-  return {
-    vertical: vertical + verticalTravel,
-    horizontal: (horizontal + horizontalTravel) % width
-  }
-}
+const getPatternLocation = ({ vertical, horizontal }, width, verticalTravel, horizontalTravel) => ({
+  vertical: vertical + verticalTravel,
+  horizontal: (horizontal + horizontalTravel) % width
+})
 
-const treesHit = pattern => {
+const treesHit = (pattern, verticalTravel, horizontalTravel) => {
   let totalTreesHit = 0
   let location = { vertical: 0, horizontal: 0 }
   while (location.vertical < pattern.length) {
     const charAtLocation = pattern[location.vertical][location.horizontal]
-    //console.log(`About to visit ${charAtLocation}`)
     if (charAtLocation === treeChar) {
       totalTreesHit++
     }
-    location = getPatternLocation(location, pattern[0].length)
-    //console.log(`Moving to location ${JSON.stringify(location)}`)
+    location = getPatternLocation(location, pattern[0].length, verticalTravel, horizontalTravel)
   }
   return totalTreesHit
 }
 
 const pattern = text.split('\n')
-const numTreesHit = treesHit(pattern)
-console.log(`The number of trees hit was ${numTreesHit}`)
+const numTreesHit = [treesHit(pattern, 1, 1), treesHit(pattern, 1, 3), treesHit(pattern, 1, 5), treesHit(pattern, 1, 7), treesHit(pattern, 2, 1)]
+const total = numTreesHit.reduce((acc, curr) => acc * curr, 1)
+console.log(`The number of trees hit was ${total}`)
