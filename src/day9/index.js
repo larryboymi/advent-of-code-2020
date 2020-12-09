27100005155afc6d3817a4defcb9c72f55ad0709
Larry Anderson <larryboymi@hotmail.com>
2020-12-09 08:03:47 -0600

    part2 day 9

--- src/day9/index.js
+++ src/day9/index.js
@@ -15,8 +15,27 @@ const findInvalid = (numbers, windowSize) => {
   }
 }
 
+const sumArray = array => array.reduce((acc, curr) => acc + curr, 0)
+
+const findContiguousOperands = (numbers, solution) => {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let k = i + 1; k < numbers.length; k++) {
+      const sum = sumArray(numbers.slice(i, k))
+      if (sum === solution) {
+        return numbers.slice(i, k)
+      } else if (sum > solution) {
+        break
+      }
+    }
+  }
+}
+
 const numbers = text.split('\n').map(Number)
 
 const invalid = findInvalid(numbers, 25)
 
 console.log(`The invalid number is ${invalid}`)
+
+const operands = findContiguousOperands(numbers, invalid)
+
+console.log(`The encryption weakness is ${Math.min(...operands) + Math.max(...operands)}`)
