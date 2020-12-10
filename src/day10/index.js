0300a866b438d62c420ccd588e35b5378316b6ca
Larry Anderson <larryboymi@hotmail.com>
2020-12-10 08:29:24 -0600

    part 2 day 10

--- src/day10/index.js
+++ src/day10/index.js
@@ -2,12 +2,8 @@ const fs = require('fs')
 const text = fs.readFileSync('input.txt', { encoding: 'utf8' })
 
 const numericSortCompare = (a, b) => a - b
-
-
-const addEnds = numbers => {
-  numbers.push(numbers[numbers.length - 1] + 3)
-  numbers.unshift(0)
-}
+const VALID_DISTANCE = 3
+const permutations = []
 
 const getDifferences = numbers => {
   const differences = new Array(3).fill(0)
@@ -18,11 +14,30 @@ const getDifferences = numbers => {
   return differences
 }
 
+const findPermutations = (numbers, begin, end) => {
+  if (!permutations[begin] && permutations[begin] !== 0) {
+    if (numbers.length === 1 && numbers[0] === end) {
+      permutations[begin] = 1
+    } else {
+      const numChoices = numbers.reduce((acc, curr) =>
+        curr > begin && curr <= (begin + VALID_DISTANCE)
+          ? acc + findPermutations(numbers, curr, end)
+          : acc
+      , 0)
+  
+      permutations[begin] = begin + VALID_DISTANCE >= end
+        ? numChoices + 1
+        : numChoices  
+    }
+  }
+  return permutations[begin]
+}
+
 const numbers = text.split('\n').map(Number).sort(numericSortCompare)
-addEnds(numbers)
+numbers.push(numbers[numbers.length - 1] + 3)
 
-console.log(`The samples are ${numbers}`)
 const differences = getDifferences(numbers)
-console.log(`the differences are ${differences}`)
-console.log(`Answer is ${differences[0] * differences[2]}`)
+console.log(`Day 10 part 1: ${(differences[0] + 1) * (differences[2])}`)
 
+const validPermutations = findPermutations(numbers, 0, numbers[numbers.length - 1] + VALID_DISTANCE)
+console.log(`Day 10 part 2: ${validPermutations}`)
