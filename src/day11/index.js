c87f3e987c77f598033456169cee6085ace50ba9
Larry Anderson <larryboymi@hotmail.com>
2020-12-13 12:00:31 -0600

    part 2 day 11

--- src/day11/index.js
+++ src/day11/index.js
@@ -4,23 +4,55 @@ const EMPTY = 'L'
 const OCCUPIED = '#'
 const FLOOR = '.'
 
+const windowOccurrences = window => Object.keys(window).reduce((acc, curr) => {
+  const isOccupied = window[curr] === OCCUPIED ? 1 : 0
+  return acc + isOccupied
+}
+, 0)
+
 const countOccurrences = (arr, val) => arr.reduce((a, v) => v === val ? a + 1 : a, 0)
 
-const windowOccurrences = window => window.reduce((acc, curr) => 
+const countConfig = config => config.reduce((acc, curr) =>
   acc + countOccurrences(curr, OCCUPIED)
-  , 0)
+, 0)
+
+const moveN = (distance, row, column) => ({row: row - distance, column})
+const moveS = (distance, row, column) => ({row: row + distance, column})
+const moveE = (distance, row, column) => ({row, column: column + distance})
+const moveW = (distance, row, column) => ({row, column: column - distance})
+const moveNW = (distance, row, column) => ({row: row - distance, column: column - distance})
+const moveSW = (distance, row, column) => ({row: row + distance, column: column - distance})
+const moveNE = (distance, row, column) => ({row: row - distance, column: column + distance})
+const moveSE = (distance, row, column) => ({row: row + distance, column: column + distance})
+
+const getSeat = directionFunc => (config, row, column) => {
+  let distance = 1
+  let location = config[row][column]
+  while (location) {
+    const indices = directionFunc(distance, row, column)
+    location = config[indices.row] && config[indices.row][indices.column] ? config[indices.row][indices.column] : null
+    if (location !== '.') {
+      break;
+    }
+    distance++
+  }
+  return location
+}
+
+const getSeats = (config, row, column) => ({
+  N: getSeat(moveN)(config, row, column),
+  NW: getSeat(moveNW)(config, row, column),
+  S: getSeat(moveS)(config, row, column),
+  SW: getSeat(moveSW)(config, row, column),
+  E: getSeat(moveE)(config, row, column),
+  NE: getSeat(moveNE)(config, row, column),
+  W: getSeat(moveW)(config, row, column),
+  SE: getSeat(moveSE)(config, row, column)
+})
 
 const takenAdjacent = (config, row, column) => {
-  const window = []
-  if (row !== 0) {
-    window.push(config[row - 1].slice(column === 0 ? column : column - 1, column + 2))
-  }
-  window.push(config[row].slice(column === 0 ? column : column - 1, column + 2))
-  if (row !== config.length - 1) {
-    window.push(config[row + 1].slice(column === 0 ? column : column - 1, column + 2))
-  }
-  const numOccurrences = windowOccurrences(window)
-  return config[row][column] === OCCUPIED ? numOccurrences - 1 : numOccurrences
+  const window = getSeats(config, row, column)
+  return windowOccurrences(window)
 }
 
 const copyArray = origArray => JSON.parse(JSON.stringify(origArray))
@@ -36,7 +68,7 @@ const takeASeat = beginningConfig => {
         if (copyConfig[i][j] === EMPTY && occurrences === 0) {
           changes++
           config[i][j] = OCCUPIED
-        } else if (copyConfig[i][j] === OCCUPIED && occurrences >= 4) {
+        } else if (copyConfig[i][j] === OCCUPIED && occurrences >= 5) {
           changes++
           config[i][j] = EMPTY
         }
@@ -48,5 +80,5 @@ const takeASeat = beginningConfig => {
 
 const configOrig = text.split('\n').map(str => Array.from(str))
 const endConfig = takeASeat(configOrig)
-console.log(`Total seats taken is ${windowOccurrences(endConfig)}`)
+console.log(`Total seats taken is ${countConfig(endConfig)}`)
 
